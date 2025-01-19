5528fad252a4dd7b05cebd0fdfbf05a62baa609d
Ankit Kumar <jangidankit722@gmail.com>
2025-01-19 18:55:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const sizeInfo = document.getElementById('sizeInfo');
+    const sizeDisplay = sizeInfo.querySelector('h1');
+
+    function updateWindowSize() {
+      sizeDisplay.textContent = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+    }
+
+    // Initial display
+    updateWindowSize();
+
+    // Event listener for window resize
+    window.addEventListener('resize', updateWindowSize);
